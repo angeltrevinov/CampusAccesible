d48447c25c65de3e727827995642a96e9585150b
Angel Treviño <angel@kobu.io>
2020-10-25 17:38:57 -0600

    Adding toast implementation for holder of intent

--- app/src/main/java/com/example/campusaccesible/BuildingAdapter.java
+++ app/src/main/java/com/example/campusaccesible/BuildingAdapter.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -14,16 +15,22 @@ import com.bumptech.glide.Glide;
 
 import java.util.List;
 
+// =========================================================
 public class BuildingAdapter extends RecyclerView.Adapter<BuildingAdapter.ViewHolder> {
 
     private List<Building> Data;
-    private Context context;
+    public Context context;
 
-    public BuildingAdapter(List<Building> itemList, Context context) {
+    // -----------------------------------------------------
+    public BuildingAdapter(
+            List<Building> itemList,
+            Context context
+    ) {
         this.context = context;
         this.Data = itemList;
     }
 
+    // -----------------------------------------------------
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(
@@ -38,31 +45,73 @@ public class BuildingAdapter extends RecyclerView.Adapter<BuildingAdapter.ViewHo
         return new ViewHolder(view);
     }
 
+    // -----------------------------------------------------
+    /**
+     * Connects the holder with the corresponding item to be
+     * displayed.
+     * @param holder the view
+     * @param position the current position in the data
+     */
     @Override
     public void onBindViewHolder(
             @NonNull ViewHolder holder,
             int position
     ) {
-        Building currentItem = Data.get(position);
+        final Building currentItem = Data.get(position);
+
+        // set text of the current building
         holder.NamBuildingHolder.setText(currentItem.getStrName());
 
-        Glide.with(context).load(currentItem.getImgUrl()).centerCrop().into(holder.img);
+        // set image of the current building
+        Glide.with(context).load(currentItem.getImgUrl())
+                .centerCrop().into(holder.img);
+
+        // set event handler for if click
+        holder.buildingHolder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: implement intent
+                showToast(currentItem.getStrName());
+            }
+        });
     }
 
+    // -------------------------------------------------
+    // TODO: Delete this method
+    public void showToast(String strName) {
+        String strMessage = "You clicked on " + strName;
+        Toast toast = Toast.makeText(this.context, strMessage, Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    // -----------------------------------------------------
     @Override
     public int getItemCount() { return this.Data.size(); }
 
 
+    // =====================================================
     public class ViewHolder extends RecyclerView.ViewHolder {
 
         ImageView img;
         TextView NamBuildingHolder;
+        View buildingHolder;
+        Context context;
 
-       public  ViewHolder(@NonNull View itemView) {
+        // -------------------------------------------------
+        /**
+         *  Just gets references of view items from our holder
+         * @param itemView
+         */
+        public  ViewHolder(@NonNull View itemView) {
             super(itemView);
 
             img = itemView.findViewById(R.id.imageBuilding);
-            this.NamBuildingHolder = itemView.findViewById(R.id.buildingNameHolder);
+            this.NamBuildingHolder = itemView.findViewById(
+                    R.id.buildingNameHolder
+            );
+            this.buildingHolder = itemView.findViewById(
+                    R.id.buildingHolder
+            );
         }
     }
 }
