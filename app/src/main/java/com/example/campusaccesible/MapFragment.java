771f1a84a4ef77e51b5d2524dfdd30db6a829468
Maggie Jimenez <maggie.jh99@gmail.com>
2020-11-14 13:47:28 -0600

    Ubicacion del Usuario

--- app/src/main/java/com/example/campusaccesible/MapFragment.java
+++ app/src/main/java/com/example/campusaccesible/MapFragment.java
@@ -11,7 +11,7 @@ import android.os.Bundle;
 
 import androidx.core.content.ContextCompat;
 import androidx.annotation.NonNull;
-import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
@@ -50,6 +50,8 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -175,7 +177,8 @@ public class MapFragment extends Fragment implements OnMapReadyCallback{
         });
 
         //Encontrar el mapa
-        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+
         mapFragment.getMapAsync(this);
 
         // Initialize fused Location
@@ -475,5 +478,15 @@ public class MapFragment extends Fragment implements OnMapReadyCallback{
         Context context = ((MainActivity) getActivity()).getApplicationContext();
         Toast toast = Toast.makeText(context, strMessage, Toast.LENGTH_SHORT);
         toast.show();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode == 44){
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                // When permission granted call method
+                getCurrentLocation();
+            }
+        }
     }
 }
